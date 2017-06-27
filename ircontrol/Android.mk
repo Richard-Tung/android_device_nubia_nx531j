82f3f0266bafb76fc7e9110c16eeb8c27bc3c575
Richard Tung <dyq@dyq.me>
2017-06-27 16:37:07 +0800

    nx531j: update IR SDK from 5.17.6.23
    Change-Id: Ib99142107c3918c2b6c36e4992c01f8692be4887

--- ircontrol/Android.mk
+++ ircontrol/Android.mk
@@ -1,12 +1,12 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE            := DATA_TP_UEIAndroidServiceSDK
+LOCAL_MODULE            := UEIAndroidServiceSDK
 LOCAL_MODULE_TAGS       := optional
 LOCAL_MODULE_CLASS      := APPS
 LOCAL_MODULE_OWNER      := nubia
 LOCAL_MODULE_SUFFIX     := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_SRC_FILES         := DATA_TP_UEIAndroidServiceSDK.apk
+LOCAL_SRC_FILES         := UEIAndroidServiceSDK.apk
 LOCAL_CERTIFICATE       := platform
 include $(BUILD_PREBUILT)
 
@@ -18,7 +18,7 @@ LOCAL_MODULE_OWNER      := nubia
 LOCAL_MODULE_SUFFIX     := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_SRC_FILES         := nubia_IRControl3.apk
 LOCAL_CERTIFICATE       := platform
-LOCAL_REQUIRED_MODULES  := DATA_TP_UEIAndroidServiceSDK
+LOCAL_REQUIRED_MODULES  := UEIAndroidServiceSDK
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
