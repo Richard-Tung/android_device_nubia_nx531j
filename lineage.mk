34a678ba8a12047294a75618ef347e804cb84284
Richard Tung <dyq@dyq.me>
2017-10-30 21:19:59 +0800

    nx531j: Remove enhanced NFC feature This is no longer used anywhere.

--- lineage.mk
+++ lineage.mk
@@ -22,9 +22,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configuration
 $(call inherit-product, device/nubia/nx531j/device.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
